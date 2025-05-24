5908335ecb20da1065c221f3fc9cd7b865590ef2
Simon125q <sz.omiecinski@gmail.com>
2025-05-24 16:06:19 +0200

    added raw data button

--- frontend/src/components/map/DataTypeSelector.vue
+++ frontend/src/components/map/DataTypeSelector.vue
@@ -8,6 +8,16 @@
     >
       {{ option.label }}
     </button>
+
+    <div class="raw-data-container">
+      <div class="raw-data-switch">
+        <span>Show raw data</span>
+        <label class="switch">
+          <input type="checkbox" v-model="showRawData" @change="toggleRawData" />
+          <span class="slider round"></span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -30,18 +40,25 @@ const options: DataTypeOption[] = [
 
 interface Props {
   modelValue: DataType;
+  showRawData: boolean; // Add showRawData prop
 }
 
 const props = defineProps<Props>();
 const emit = defineEmits<{
   (e: 'update:modelValue', value: DataType): void;
+  (e: 'update:showRawData', value: boolean): void; // Emit event for raw data toggle
 }>();
 
 const selectedType = ref(props.modelValue);
+const showRawData = ref(props.showRawData); // Initialize from prop
 
 function selectType(type: DataType) {
   selectedType.value = type;
   emit('update:modelValue', type);
+}
+
+function toggleRawData() {
+  emit('update:showRawData', showRawData.value);
 }
 </script>
 
@@ -75,4 +92,86 @@ function selectType(type: DataType) {
   color: white;
   border-color: #0d8bf2;
 }
+
+.raw-data-container {
+  background-color: white;
+  border-radius: 8px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.raw-data-switch {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 14px;
+  color: #333;
+
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 30px;
+  height: 17px;
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 13px;
+  width: 13px;
+  left: 2px;
+  bottom: 2px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked+.slider {
+  background-color: #2196F3;
+}
+
+input:focus+.slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked+.slider:before {
+  -webkit-transform: translateX(13px);
+  -ms-transform: translateX(13px);
+  transform: translateX(13px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 17px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
 </style>
+
